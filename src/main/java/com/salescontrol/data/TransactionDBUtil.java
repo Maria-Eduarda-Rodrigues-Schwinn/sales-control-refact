c628b82ce43130bbb4e89f62b99409e54f8f2d48
Maria Eduarda Schwinn <maria.eduarda.schwinn@gmail.com>
2025-06-18 22:19:52 -0300

    apply try-with-resources and remove entityManager shared

--- src/main/java/com/salescontrol/data/TransactionDBUtil.java
+++ src/main/java/com/salescontrol/data/TransactionDBUtil.java
@@ -8,46 +8,41 @@ import java.util.function.Function;
 public class TransactionDBUtil {
 
   public static void execute(Consumer<EntityManager> action) {
-    EntityManager em = JPAUtil.getEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    try {
-      tx.begin();
-      action.accept(em);
-      tx.commit();
-    } catch (Exception e) {
-      if (tx.isActive()) {
-        tx.rollback();
+    try (EntityManager em = JPAUtil.getEntityManager()) {
+      EntityTransaction tx = em.getTransaction();
+      try {
+        tx.begin();
+        action.accept(em);
+        tx.commit();
+      } catch (Exception e) {
+        if (tx.isActive()) {
+          tx.rollback();
+        }
+        throw e;
       }
-      throw e;
-    } finally {
-      em.close();
     }
   }
 
   public static <R> R executeWithResult(Function<EntityManager, R> action) {
-    EntityManager em = JPAUtil.getEntityManager();
-    EntityTransaction tx = em.getTransaction();
-    try {
-      tx.begin();
-      R result = action.apply(em);
-      tx.commit();
-      return result;
-    } catch (Exception e) {
-      if (tx.isActive()) {
-        tx.rollback();
+    try (EntityManager em = JPAUtil.getEntityManager()) {
+      EntityTransaction tx = em.getTransaction();
+      try {
+        tx.begin();
+        R result = action.apply(em);
+        tx.commit();
+        return result;
+      } catch (Exception e) {
+        if (tx.isActive()) {
+          tx.rollback();
+        }
+        throw e;
       }
-      throw e;
-    } finally {
-      em.close();
     }
   }
 
   public static <R> R executeWithoutTransaction(Function<EntityManager, R> action) {
-    EntityManager em = JPAUtil.getEntityManager();
-    try {
+    try (EntityManager em = JPAUtil.getEntityManager()) {
       return action.apply(em);
-    } finally {
-      em.close();
     }
   }
 }
