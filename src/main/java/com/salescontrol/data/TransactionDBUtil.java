6f7b0d2c870b6e8936fb98bda6877e95f0a0c2b9
Maria Eduarda Schwinn <maria.eduarda.schwinn@gmail.com>
2025-06-18 22:35:05 -0300

    add exception class that may occur when accessing the database

--- src/main/java/com/salescontrol/data/TransactionDBUtil.java
+++ src/main/java/com/salescontrol/data/TransactionDBUtil.java
@@ -1,5 +1,6 @@
 package com.salescontrol.data;
 
+import com.salescontrol.exception.DaoException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import java.util.function.Consumer;
@@ -8,41 +9,39 @@ import java.util.function.Function;
 public class TransactionDBUtil {
 
   public static void execute(Consumer<EntityManager> action) {
-    try (EntityManager em = JPAUtil.getEntityManager()) {
-      EntityTransaction tx = em.getTransaction();
-      try {
-        tx.begin();
-        action.accept(em);
-        tx.commit();
-      } catch (Exception e) {
-        if (tx.isActive()) {
-          tx.rollback();
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                action.accept(em);
+                tx.commit();
+            } catch (Exception e) {
+                if (tx.isActive()) tx.rollback();
+                throw new DaoException("Error executing a transaction on the database: ", e);
+            }
         }
-        throw e;
-      }
     }
-  }
 
   public static <R> R executeWithResult(Function<EntityManager, R> action) {
-    try (EntityManager em = JPAUtil.getEntityManager()) {
-      EntityTransaction tx = em.getTransaction();
-      try {
-        tx.begin();
-        R result = action.apply(em);
-        tx.commit();
-        return result;
-      } catch (Exception e) {
-        if (tx.isActive()) {
-          tx.rollback();
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            try {
+                tx.begin();
+                R result = action.apply(em);
+                tx.commit();
+                return result;
+            } catch (Exception e) {
+                if (tx.isActive()) tx.rollback();
+                throw new DaoException("Error executing transaction with return in database: ", e);
+            }
         }
-        throw e;
-      }
     }
-  }
 
   public static <R> R executeWithoutTransaction(Function<EntityManager, R> action) {
-    try (EntityManager em = JPAUtil.getEntityManager()) {
-      return action.apply(em);
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            return action.apply(em);
+        } catch (Exception e) {
+            throw new DaoException("Error executing query without transaction on database: ", e);
+        }
     }
-  }
 }
